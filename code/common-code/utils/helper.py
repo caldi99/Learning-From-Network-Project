067c455554031a02b307ee334be0b666ea199763
Francesco <francesco.caldivezzi@hotmail.it>
2022-11-23 20:59:54 +0100

    script dataset

---
+++ code/common-code/utils/helper.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import os
+from pathlib import Path
+
+class Helper:
+    def merge_dataset(self,path_list_csv_to_merge):
+        path_files = self.get_path_csv_files(path_list_csv_to_merge)
+
+        list_dataframes_to_merge = []
+
+        #get dataframes to merge
+        for path_file in path_files:
+            list_dataframes_to_merge.append(pd.read_csv(path_file))
+        
+        #merge dataframes
+        return pd.concat(list_dataframes_to_merge,ignore_index=True)
+
+    def save_dataframe_to_csv(self, dataframe, path_to_write_csv):
+        #save dataframe to correct path
+        dataframe.to_csv(Path(path_to_write_csv),index=False)
+
+
+    def get_path_csv_files(self,path_list_csv):
+        # list paths of files to return
+        list_paths = []
+
+        #list of names of the files
+        list_name_files = os.listdir(path_list_csv)
+        
+        for file_name in list_name_files:
+            list_paths.append(os.path.join(path_list_csv,file_name))
+        
+        return list_paths
